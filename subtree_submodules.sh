2da9b1f6613f8d20ed9d06b2d5f1e8c9294e572f
Chris Wornell <fro.888@gmail.com>
2014-03-27 19:39:56 -0500

    Removed .gitmodules

---
+++ subtree_submodules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git subtree add --prefix bundle/ctrlp https://github.com/kien/ctrlp.vim.git master --squash
+git subtree add --prefix bundle/nerdtree https://github.com/scrooloose/nerdtree master --squash
+git subtree add --prefix bundle/supertab https://github.com/ervandew/supertab.git master --squash
+git subtree add --prefix bundle/syntastic https://github.com/scrooloose/syntastic.git master --squash
+git subtree add --prefix bundle/tabular https://github.com/godlygeek/tabular master --squash
+git subtree add --prefix bundle/tagbar https://github.com/majutsushi/tagbar.git master --squash
+git subtree add --prefix bundle/vim-commentary https://github.com/tpope/vim-commentary.git master --squash
+git subtree add --prefix bundle/vim-misc https://github.com/xolox/vim-misc master --squash
+git subtree add --prefix bundle/vim-easytags https://github.com/xolox/vim-easytags master --squash
+git subtree add --prefix bundle/vim-endwise https://github.com/tpope/vim-endwise.git master --squash
+git subtree add --prefix bundle/vim-fugitive https://github.com/tpope/vim-fugitive.git master --squash
+git subtree add --prefix bundle/vim-markdown https://github.com/plasticboy/vim-markdown.git master --squash
+git subtree add --prefix bundle/vim-ruby https://github.com/vim-ruby/vim-ruby.git master --squash
