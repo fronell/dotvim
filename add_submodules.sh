07b0e12621025dc2f4fc867b1b3f95ac44b82289
Chris Wornell <fro.888@gmail.com>
2014-03-27 19:34:12 -0500

    Removed all submodules

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-git submodule add https://github.com/vim-ruby/vim-ruby.git bundle/vim-ruby
-git submodule add https://github.com/tpope/vim-fugitive.git bundle/vim-fugitive
+git submodule add https://github.com/kien/ctrlp.vim.git bundle/ctrlp
+git submodule add https://github.com/scrooloose/nerdtree bundle/nerdtree
+git submodule add https://github.com/ervandew/supertab.git bundle/supertab
 git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
 git submodule add https://github.com/godlygeek/tabular bundle/tabular
-git submodule add https://github.com/kien/ctrlp.vim.git bundle/ctrlp
-git submodule add https://github.com/tpope/vim-endwise.git bundle/vim-endwise
-git submodule add https://github.com/scrooloose/nerdtree bundle/nerdtree
 git submodule add https://github.com/majutsushi/tagbar.git bundle/tagbar
 git submodule add https://github.com/tpope/vim-commentary.git bundle/vim-commentary
-git submodule add https://github.com/fholgado/minibufexpl.vim.git bundle/minibufexpl
-git submodule add https://github.com/ervandew/supertab.git bundle/supertab
-git submodule add https://github.com/gmarik/Vundle.vim.git bundle/vundle
+git submodule add https://github.com/xolox/vim-misc bundle/vim-misc
+git submodule add https://github.com/xolox/vim-easytags bundle/vim-easytags
+git submodule add https://github.com/tpope/vim-endwise.git bundle/vim-endwise
+git submodule add https://github.com/tpope/vim-fugitive.git bundle/vim-fugitive
+git submodule add https://github.com/plasticboy/vim-markdown.git bundle/vim-markdown
+git submodule add https://github.com/vim-ruby/vim-ruby.git bundle/vim-ruby
