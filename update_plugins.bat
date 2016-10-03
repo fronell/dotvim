2d4df842b4fb8bf84ebb17d732752eba983fa4a6
Chris Wornell <fro.888@gmail.com>
2016-10-02 21:02:59 -0500

    Removed 'vim-' from plugin names in bundle folder; Removed nerdcomment & commentary plugins; Added tcomment; Updated update_plugins.bat; Added ftplugin/python_fun.vim to improve motions for Python such as selecting an entire function that has spaces in it

--- update_plugins.bat
+++ update_plugins.bat
@@ -9,37 +9,35 @@ REM AutoComplPop: Fork that does not automatically select first item in popup me
 REM More here: https://bitbucket.org/ns9tks/vim-autocomplpop/issue/53/make-autoselection-of-the-first-item
 git clone https://github.com/dirkwallenstein/vim-autocomplpop %TEMPDIR%\autocomplpop 
 REM Bbye: Deleting a buffer without closing the window
-git clone https://github.com/moll/vim-bbye.git %TEMPDIR%\vim-bbye 
-REM Commentary: Makes commenting fun! (Inserts a space after comment)
-git clone https://github.com/tpope/vim-commentary.git %TEMPDIR%\vim-commentary 
+git clone https://github.com/moll/vim-bbye.git %TEMPDIR%\bbye 
 REM ctrlsf: An ack/ag/pt powered search and view tool
 git clone https://github.com/dyng/ctrlsf.vim.git %TEMPDIR%\ctrlsf
 REM Ctrl-P: Fuzzy finder for files/tags/buffers
 git clone https://github.com/ctrlpvim/ctrlp.vim %TEMPDIR%\ctrlp 
 REM Endwise: Automatically closes programming structures like adding an end to Ruby blocks
-git clone https://github.com/tpope/vim-endwise.git %TEMPDIR%\vim-endwise 
+git clone https://github.com/tpope/vim-endwise.git %TEMPDIR%\endwise 
 REM Fugitive: Git management in Vim
-git clone https://github.com/tpope/vim-fugitive.git %TEMPDIR%\vim-fugitive 
+git clone https://github.com/tpope/vim-fugitive.git %TEMPDIR%\fugitive 
 REM gitv: Provides a commit browser for the repo or a specific file
 git clone https://github.com/gregsexton/gitv.git %TEMPDIR%\gitv 
+REM jedi-vim: awesome Python autocompletion with VIM
+git clone --recursive https://github.com/davidhalter/jedi-vim.git %TEMPDIR%\jedi-vim
 REM L9: Dependency for autocomplpop
 git clone https://github.com/vim-scripts/L9.git %TEMPDIR%\L9 
-REM NERD Commenter: Makes commenting fun! (No space after comment)
-git clone https://github.com/scrooloose/nerdcommenter.git %TEMPDIR%\nerdcommenter 
 REM The Platinum Searcher: Vim interface for pt
 git clone https://github.com/nazo/pt.vim.git %TEMPDIR%\pt
 REM Repeat: Required to repeat actions with '.' in surround and commentary plugins
-git clone https://github.com/tpope/vim-repeat.git %TEMPDIR%\vim-repeat 
+git clone https://github.com/tpope/vim-repeat.git %TEMPDIR%\repeat 
 REM Ruby: Provides general support for Ruby like text-objects/motions/omnicompletion
-git clone https://github.com/vim-ruby/vim-ruby.git %TEMPDIR%\vim-ruby 
+git clone https://github.com/vim-ruby/vim-ruby.git %TEMPDIR%\ruby 
 REM Ruby Text-Objects: Provides Ruby text-objects for selecting text in and around blocks
-git clone https://github.com/nelstrom/vim-textobj-rubyblock.git %TEMPDIR%\vim-textobj-rubyblock 
+git clone https://github.com/nelstrom/vim-textobj-rubyblock.git %TEMPDIR%\textobj-rubyblock 
 REM Space Jam: This plugin automatically removes trailing whitespace for ruby, python, javascript, et al
 git clone https://github.com/rondale-sc/vim-spacejam.git %TEMPDIR%\spacejam 
 REM Solarized: Colorscheme
-git clone https://github.com/altercation/vim-colors-solarized.git %TEMPDIR%\vim-colors-solarized 
+git clone https://github.com/altercation/vim-colors-solarized.git %TEMPDIR%\colors-solarized 
 REM Surround: Makes handling surrounding structures like '' () [] fun!
-git clone https://github.com/tpope/vim-surround.git %TEMPDIR%\vim-surround 
+git clone https://github.com/tpope/vim-surround.git %TEMPDIR%\surround 
 REM Syntastic: Syntax checker 
 git clone https://github.com/scrooloose/syntastic.git %TEMPDIR%\syntastic 
 REM Tabular: Makes aligning text fun!
@@ -47,7 +45,7 @@ git clone https://github.com/godlygeek/tabular.git %TEMPDIR%\tabular
 REM Tagbar: Displays tags ordered by scope
 git clone https://github.com/majutsushi/tagbar.git %TEMPDIR%\tagbar 
 REM Text-Objects: Dependency for vim-textobj-rubyblock
-git clone https://github.com/kana/vim-textobj-user.git %TEMPDIR%\vim-textobj-user 
+git clone https://github.com/kana/vim-textobj-user.git %TEMPDIR%\textobj-user 
 REM Togglelist: Allows for a binding to toggle the quicklist and locationlist
 git clone https://github.com/milkypostman/vim-togglelist.git %TEMPDIR%\togglelist 
 REM Tmux-Navigator: Use vim key bindings to navigate vim windows and tmux windows
